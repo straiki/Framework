15ae64d792dfdb37336f68caa5846745c5b74e20
Schmutzka <tomas.vot@gmail.com>
2012-06-02 18:01:58 +0200

    StaticCodebook Form

---
+++ Forms/StaticCodebookForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Schmutzka\Forms;
+
+class StaticCodebookForm extends Form
+{
+
+	/** @var string */
+	public $flashText = "Údaje uloženy.";
+	
+
+	/** @var \Models\Codebook */
+	private $model;
+	
+
+	public function __construct($model)
+	{
+		parent::__construct();
+		$this->model = $model;
+	}
+
+
+	public function build()
+	{
+		parent::build();
+
+		$this->addSubmit("submit", "Uložit");
+
+		$defaults = $this->model->all()->fetchPairs("name", "value");
+		$this->setDefaults($defaults);
+	}
+	
+
+	public function process(StaticCodebookForm $form)
+	{
+		$values = $form->values;
+		foreach ($values as $key => $name) {
+			$this->model->upsert(
+				array("name" => $key, "value" => $name), 
+				array("name" => $key)
+			);
+		}
+
+		$this->flashMessage($this->flashText, "flash-success");
+		$this->redirect("this");
+	}
+
+}
