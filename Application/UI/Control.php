b8226ce9cb2e2901fd64b871d479f15a7ec10e0a
Schmutzka <tomas.vot@gmail.com>
2012-06-02 16:15:04 +0200

    Basic control improved

--- Application/UI/Control.php
+++ Application/UI/Control.php
@@ -2,15 +2,47 @@
 
 /**
  * @author   Jan Tvrdík
+ * @author   Tomáš Votruba
  */
 
 namespace Schmutzka\Application\UI;
+
+use Schmutzka\Templates\MyHelpers,
+	Schmutzka\Templates\MyMacros;
 
 class Control extends \Nette\Application\UI\Control
 {
 
 	/**
-	 * Automatically registers template file
+	 * Context shortcut
+	 */
+	final public function getContext()
+	{
+		return $this->parent->context;
+	}
+
+
+	/**
+	 * Model shortcut 
+	 */
+	final public function getModels()
+	{
+		return $this->context->models;
+	}
+
+
+	/**
+	 * FlashMessage component
+	 * @return \Components\FlashMessageControl
+	 */
+	protected function createComponentFlashMessage()
+	{
+		return new \Components\FlashMessageControl;
+	}
+
+
+	/**
+	 * Automatically registers template file and all filters
 	 * @param string
 	 * @return Nette\Templates\FileTemplate
 	 */
@@ -19,7 +51,28 @@ class Control extends \Nette\Application\UI\Control
 		$template = parent::createTemplate($class);
 		$template->setFile($this->getTemplateFilePath());
 
+		// Latte, Haml, macros
+		$this->templatePrepareFilters($template);
+		
+		// helpers
+		$helpers = new MyHelpers($this->parent->context, $this->parent->presenter);
+		$template->registerHelperLoader(array($helpers, "loader"));
+
 		return $template;
+	}
+
+
+	/**
+	 * Register filters
+	 */
+	public function templatePrepareFilters($template)
+	{
+		$latte = new \Nette\Latte\Engine;
+
+		MyMacros::install($latte->compiler);
+
+		$template->registerFilter(new \Nette\Templating\Filters\Haml);
+		$template->registerFilter($latte);
 	}
 
 
@@ -31,7 +84,8 @@ class Control extends \Nette\Application\UI\Control
 	{
 		$reflection = $this->getReflection();
 		$dir = dirname($reflection->getFileName());
-		$filename = $reflection->getShortName() . '.latte';
+		$filename = $reflection->getShortName() . ".latte";
+
 		return $dir . \DIRECTORY_SEPARATOR . $filename;
 	}
 
