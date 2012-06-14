535c1d8d4bb8e82ae63b3515fa3ac0be72d86482
Schmutzka <tomas.vot@gmail.com>
2012-06-14 12:00:43 +0200

    Forms\InsertUpdateForm submit caption now changeable

--- Forms/InsertUpdateForm.php
+++ Forms/InsertUpdateForm.php
@@ -6,9 +6,6 @@ use Schmutzka\Forms\Form;
 
 class InsertUpdateForm extends  Form
 {
-	/** @ar object */
-	private $model;
-
 
 	/** @persistent */
 	public $id;
@@ -17,30 +14,45 @@ class InsertUpdateForm extends  Form
 	public $flashText = "Záznam byl úspěšně uložen.";
 
 	/** @var string */
-	public $redirectInsert = "default";
+	public $redirectInsert = "this";
 
 	/** @var string */
-	public $redirectUpdate = "default";
+	public $redirectUpdate = "this";
 
-	/** @var array */
-	public $onValues;
+	/** @var string */
+	public $insertCaption = "Přidat";
+
+	/** @var string */
+	public $updateCaption = "Uložit";
 
 
-	public function __construct($model, $id)
+	/** @persistent */
+	private $userId;
+
+	/** @var object */
+	private $model;
+
+
+	public function __construct($model, $id, $userId = NULL)
 	{
 		parent::__construct();
 		$this->model = $model;
 		$this->id = $id;
+		$this->userId = $userId;
 	}
 
 
 	public function build()
 	{
 		parent::build();
-		$this->addSubmit();
+		$this->addSubmit("send");
 
 		if ($this->id) {
+			$this["send"]->caption = $this->updateCaption;
 			$this->setDefaults($this->model->item($this->id));
+		}
+		else {
+			$this["send"]->caption = $this->insertCaption;
 		}
 	}
 
@@ -49,19 +61,19 @@ class InsertUpdateForm extends  Form
 	{
 		$values = $form->values;
 
-		if ($this->onValues) { // callback() doesn't return any values
-			$values = $this->parent->{$this->onValues}($values);
+		if ($this->userId) {
+			$values["user_id"] = $this->userId;
 		}
 
 		if ($this->id) {
 			$this->model->update($values, $this->id);
 			$this->flashMessage($this->flashText, "flash-success");
-			$this->redirect($this->redirectUpdate);
+			$this->redirect($this->redirectUpdate, array("id" => NULL));
 		}
 		else {
 			$this->model->insert($values);
 			$this->flashMessage($this->flashText, "flash-success");
-			$this->redirect($this->redirectInsert);
+			$this->redirect($this->redirectInsert, array("id" => NULL));
 		}
 	}
 
