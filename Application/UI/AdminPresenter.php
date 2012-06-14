31a40e2f378fb00a3f0c023cf5b2425cd9611757
Schmutzka <tomas.vot@gmail.com>
2012-06-14 12:05:38 +0200

    UI\AdminPresenter logged user control fixed

--- Application/UI/AdminPresenter.php
+++ Application/UI/AdminPresenter.php
@@ -1,31 +1,29 @@
 <?php
 
-/**
- * Presenter with base functions in Admin module
- */
-
 namespace Schmutzka\Application\UI;
 
-use Nette\Security\User;
-
-class AdminPresenter extends \BasePresenter // if exists
+class AdminPresenter extends \BasePresenter
 {
 
 	/** @var redirect unlogged */
 	public $unloggedRedirect = ":Front:Homepage:default";
 
+
 	public function startup()
 	{
 		parent::startup();
 
-        if(!$this->user->isLoggedIn()) {
-            $this->redirect($this->unloggedRedirect);
+		$currentSite = (ltrim($this->name . ":" . $this->view, "Admin:"));
+        if (!$this->user->isLoggedIn()) {
+			if ($this->unloggedRedirect != $currentSite) {
+				$this->flashMessage("Pro přístup do této sekce se musíte přihlásit.", "flash-info");
+				$this->redirect($this->unloggedRedirect);
+			}
         }
-		elseif(!$this->user->isAllowed($this->name, $this->action)) {
-			$this->flashMessage("Na vstup do této sekce nemáte dostatečné oprávnění!", "warning");
+		elseif (!$this->user->isAllowed($this->name, $this->action)) {
+			$this->flashMessage("Na vstup do této sekce nemáte dostatečné oprávnění!", "flash-warning");
 			$this->redirect($this->unloggedRedirect);
         }
-	
 	}
 
 }
