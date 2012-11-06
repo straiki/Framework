15329f571364c935ba0cf1dbf63cea01a7e7a323
Schmutzka <tomas.vot@gmail.com>
2012-11-06 11:49:02 +0100

    UI\AdminPresenter -> acl now optional

--- Application/UI/AdminPresenter.php
+++ Application/UI/AdminPresenter.php
@@ -2,25 +2,27 @@
 
 namespace Schmutzka\Application\UI;
 
-class AdminPresenter extends \BasePresenter
+class AdminPresenter extends \FrontModule\BasePresenter
 {
-
 	/** @var string */
-	public $unloggedRedirect = ":Front:Homepage:default";
+	protected $unloggedRedirect = ":Admin:Homepage:default";
+
+	/** @var bool */
+	protected $useAcl = FALSE;
 
 
 	public function startup()
 	{
 		parent::startup();
 
-		$currentSite = (ltrim($this->name . ":" . $this->view, "Admin:"));
+		$currentSite = ":" . $this->name . ":" . $this->view;
         if (!$this->user->isLoggedIn()) {
 			if ($this->unloggedRedirect != $currentSite) {
 				$this->flashMessage("Pro přístup do této sekce se musíte přihlásit.", "flash-info");
 				$this->redirect($this->unloggedRedirect);
 			}
-        }
-		elseif (!$this->user->isAllowed($this->name, $this->action)) {
+
+        } elseif ($this->useAcl && !$this->user->isAllowed($this->name, $this->action)) {
 			$this->flashMessage("Na vstup do této sekce nemáte dostatečné oprávnění!", "flash-warning");
 			$this->redirect($this->unloggedRedirect);
         }
