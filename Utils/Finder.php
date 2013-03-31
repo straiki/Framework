e71621e06d41e644ca5eba50853a0a4e185d48b8
Schmutzka <tomas.vot@gmail.com>
2013-03-31 19:23:33 +0200

    new class Utils\Finder

---
+++ Utils/Finder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Schmutzka\Utils;
+
+use Nette;
+
+class Finder extends Nette\Utils\Finder
+{
+
+	/**
+	 * Convert found files to array list
+	 * @return array
+	 */
+	public function toArray()
+	{
+		$array = array();
+		foreach ($this as $name => $info) {
+			$uid = pathinfo($info->getFilename(), PATHINFO_FILENAME);
+			$array[$uid] = $name;
+		}
+
+		return $array;
+	}
+
+}
