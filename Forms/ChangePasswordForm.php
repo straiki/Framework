a56ce8f52f62e5c5c1af9af3000593abb8c380f5
Schmutzka <tomas.vot@gmail.com>
2013-03-31 19:29:07 +0200

    ChangePasswordForm removed context dependency

--- Forms/ChangePasswordForm.php
+++ Forms/ChangePasswordForm.php
@@ -2,79 +2,77 @@
 
 namespace Schmutzka\Forms;
 
+use Nette;
+use Models;
+use Schmutzka\Utils\Password;
+use Schmutzka;
+
 class ChangePasswordForm extends Form
 {
-
-	/** \SystemContainer */
-	private $context;
-
-	/** \Models\User */
-	private $userModel;
-
-
 	/** @var int */
 	public $minLength = 5;
 
+	/** @var Schmutzka\Models\User */
+	private $userModel;
+
+	/** @var Nette\Security\User */
+	private $user;
+
 	/** @var string */
-	public $tableName = "user";
+	private $salt;
 
 
 	/**
-	 * Build the form
+	 * @param Schmutzka\Models\User
+	 * @param Nette\Security\User
+	 * @param Schmutzka\Services\ParamService
 	 */
-	public function __construct(\Nette\DI\Container $context)
+	public function __construct(Schmutzka\Models\User $userModel, Nette\Security\User $user, Schmutzka\Config\ParamService $paramService)
 	{
 		parent::__construct();
+		$this->userModel = $userModel;
+		$this->user = $user;
+		$this->salt = (isset($paramService->params["salt"])) ? $paramService->params["salt"] : NULL;
+	}
 
-		$this->context = $context;
-		$this->userModel = $context->models->user;
 
-		if (isset($context->params["passwordMinLength"])) { 
-			$this->minLength = $context->params["passwordMinLength"];
-		}
+	public function build()
+	{
+		parent::build();
 
-		$this->addPassword("oldPassword","Současné heslo:",15)
-			->addRule(Form::FILLED,"Zadejte současné heslo");
-		$this->addPassword("password","Nové heslo:",15)
+		$this->addPassword("oldPassword","Staré heslo:")
+			->addRule(Form::FILLED,"Zadejte staré heslo");
+		$this->addPassword("password","Nové heslo:")
 			->addRule(Form::FILLED,"Zadejte nové heslo");
 
 		if ($this->minLength) {
 			$this["password"]->addRule(Form::MIN_LENGTH, "Heslo musí mít aspoň %d znaků", $this->minLength);
 		}
 
-        $this->addPassword('passwordCheck',"Znovu nové heslo:",15)
-          ->addRule(Form::FILLED,"Zadejte heslo k ověření")
-          ->addRule(Form::EQUAL,"Hesla musejí být schodná",$this["password"]);   
+		$this->addPassword('passwordCheck',"Znovu nové heslo:")
+			->addRule(Form::FILLED,"Zadejte heslo k ověření")
+			->addRule(Form::EQUAL,"Hesla musejí být schodná",$this["password"]);   
 		$this->addSubmit("send","Změnit heslo")
 			->setAttribute("class","btn btn-primary");
 	}
 
 
-	/**
-	 * Change password
-	 * @form
-	 */
 	public function process(ChangePasswordForm $form)
 	{
 		$values = $form->values;
-
-		$user = $this->presenter->user;
-		$record = $this->userModel->item(array("id" => $user->id, "password", sha1($values["oldPassword"])));
-
+		$record = $this->userModel->item(array(
+			"id" => $this->user->id,
+			"password" => Password::saltHash($values["oldPassword"], $this->salt)
+		));
+	
 		if (count($record)) {
-			if ($values["password"] == $values["passwordCheck"] AND strlen($values["password"]) >= $this->minLength) {
-				$this->userModel->update(array(
-					"password" => sha1($values["password"])
-				), $user->id);
+			$array["password"] = Password::saltHash($values["password"], $this->salt);
+			$this->userModel->update($array, $this->user->id);
 
-				$this->flashMessage("Heslo bylo úspěšně změněno.", "flash-success");
-			}
-			else {
-				$this->flashMessage("Hesla se neshodují, nebo je příliš krátké.","flash-error");
-			}
-		}
-		else {
-			$this->flashMessage("Zadali jste chybně současné heslo.","flash-error");
+			$this->flashMessage("Heslo bylo úspěšně změněno.", "flash-success");
+
+		} else {
+			$this->flashMessage("Zadali jste chybně staré heslo.","flash-error");
 		}
 
 		$this->redirect("this");
