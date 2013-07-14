116b5a5a115a8a3dce6e40a6165cb0cf51ad9816
Schmutzka <tomas.vot@gmail.com>
2013-07-14 12:06:05 +0200

    ModuleRouterFactory - autosetup for page and article id => url added

--- Application/Routers/ModuleRouterFactory.php
+++ Application/Routers/ModuleRouterFactory.php
@@ -11,6 +11,15 @@ class ModuleRouterFactory
 	/** @inject @var Schmutzka\Config\ParamService */
 	public $paramService;
 
+	/** @inject @var Schmutzka\Models\Page */
+	public $pageModel;
+
+	/** @inject @var Schmutzka\Models\Article */
+	public $articleModel;
+
+	/** @inject @var Nette\Caching\Cache */
+	public $cache;
+
 	/** @var array */
 	protected $customModules = array();
 
@@ -22,8 +31,23 @@ class ModuleRouterFactory
 		$router[] = new Route("index.php", "Front:Homepage:default", Route::ONE_WAY);
 		$router[] = new Route("index.php", "Admin:Homepage:default", Route::ONE_WAY);
 
-		$activeModules = array_keys($this->paramService->getActiveModules());
-		$router[] = new Route("<module admin|" . Name::upperToDashedLower(implode($activeModules + $this->customModules, "|")) . ">/<presenter>/<action>[/<id>]", "Homepage:default");
+		$activeModules = $this->paramService->getActiveModules();
+		$activeModulesKeys = array_keys($activeModules);
+		$router[] = new Route("<module admin|" . Name::upperToDashedLower(implode($activeModulesKeys + $this->customModules, "|")) . ">/<presenter>/<action>[/<id>]", "Homepage:default");
+
+		if (isset($activeModules["page"])) {
+			$frontRouter = $router[] = new RouteList("Front");
+			$frontRouter[] = new PairsRoute("<id>", "Page:detail", NULL, $this->pageModel, $this->cache, $columns = array(
+				"id", "url"
+			));
+		}
+
+		if (isset($activeModules["article"])) {
+			$frontRouter = $router[] = new RouteList("Front");
+			$frontRouter[] = new PairsRoute("<id>", "Article:detail", NULL, $this->articleModel, $this->cache, $columns = array(
+				"id", "url"
+			));
+		}
 
 		return $router;
 	}
@@ -40,5 +64,7 @@ class ModuleRouterFactory
 
 		return $frontRouter;
 	}
+
+
 
 }
